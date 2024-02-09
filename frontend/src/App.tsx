3b9be4d1bd31cfccab4ab3179cb640e301f65fb5
Ankit Dash <ankitdash2019@gmail.com>
2024-02-09 21:57:32 +0530

    start frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <div className="mx-auto flex px-4 py-16 sm:px-6 sm:py-28 lg:px-2 items-center justify-center">
+                <div className="text-6xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+                    Nebula Nest
+                </div>      
+            </div>
+        </>
+    )
+}
