61895e32f997bec914926de67c6362a3d226a558
Ankit Dash <ankitdash2019@gmail.com>
2024-02-18 23:40:18 +0530

    fix

--- backend/src/functions/aws.ts
+++ backend/src/functions/aws.ts
@@ -1,50 +1,57 @@
 import fs from "fs"
 import path from "path"
-import AWS from 'aws-sdk'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import dotenv from 'dotenv'
 
-dotenv.config({ path: './../../.env' })
-
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS,
-    endpoint: process.env.AWS_URL
+dotenv.config({ path: '../.env' })
+console.log(process.env.AWS_ACCESS_KEY_ID)
+const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+    },
 })
 
 export function getFilePaths(dirPath: string): string[] {
-    let filePaths: string[] = [];
+    let filePaths: string[] = []
 
     function walk(dir: string) {
-        const files = fs.readdirSync(dir);
+        const files = fs.readdirSync(dir)
         for (let i = 0; i < files.length; i++) {
-            const filePath = path.join(dir, files[i]);
-            const stat = fs.statSync(filePath);
+            const filePath = path.join(dir, files[i])
+            const stat = fs.statSync(filePath)
             if (stat.isDirectory()) {
-                walk(filePath);
+                walk(filePath)
             } else {
                 filePaths.push(path.resolve(filePath))
             }
         }
     }
 
-    walk(dirPath);
-    return filePaths;
+    walk(dirPath)
+    return filePaths
 }
 
 export async function uploadToS3(dirPath: string[]) {
     try {
+        const urls = []
         for (const filePath of dirPath) {
             const fileContent = fs.readFileSync(filePath)
             const uploadParams = {
                 Bucket: 'nebula-nest',
-                Key: filePath,
+                Key: path.basename(filePath),
                 Body: fileContent
             }
-            const response = await s3.upload(uploadParams).promise()
+            const command = new PutObjectCommand(uploadParams)
+            const response = await s3Client.send(command)
             console.log(response)
+            const url = await getSignedUrl(s3Client, new PutObjectCommand({ ...uploadParams }))
+            urls.push(url)
         }
+        return urls
     } catch (error) {
-        console.log("hello bro, so many errors")
-        console.error(error);
+        console.error(error)
     }
 }
