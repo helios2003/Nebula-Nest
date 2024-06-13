e489dcf4ebe10f146005332c17499e8842cc18bb
Ankit Dash <ankitdash2019@gmail.com>
2024-06-13 13:28:23 +0530

    revert

--- deploy/src/functions/build.ts
+++ deploy/src/functions/build.ts
@@ -1,11 +1,28 @@
 import { exec } from 'child_process';
 import path from 'path';
 
-export function buildProject(projectId: string) {
-    const dockerFileDir = path.resolve(__dirname, '..', '..');
-    const filesParentDir = path.resolve(__dirname, '..', '..', '..', 'build', projectId);
-    const buildShellScript = path.join(dockerFileDir, 'Dockerfile');
-    console.log("building shell script", buildShellScript);
-    console.log("project id path", filesParentDir);
-    exec(`bash ./build.sh "${buildShellScript}" "${filesParentDir}"`);
+export async function buildProject(projectId: string) {
+    const dockerFile = path.resolve(__dirname, '..', '..', 'Dockerfile');
+    const buildFile = path.resolve(__dirname, '..', '..', 'build.sh');
+    const projectDir = path.resolve(__dirname, '..', '..', '..', 'build', projectId);
+
+    console.log('Dockerfile path', dockerFile);
+    console.log("building shell script", buildFile);
+    console.log("project id path", projectDir);
+
+    return new Promise((resolve, reject) => {
+        exec(`bash "${buildFile}" "${dockerFile}" "${projectDir}"`, (error, stdout, stderr) => {
+            if (error) {
+                reject(`Error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                reject(`stderr: ${stderr}`);
+                return;
+            }
+            resolve(stdout.trim());
+        });
+    }).catch(Error => {
+        console.log(Error);
+    });
 }
